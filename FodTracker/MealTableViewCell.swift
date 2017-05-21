a54933474ae34b58625e1f7a8cf5ed83b7b96544
bqmoreland <bqmoreland@gmail.com>
2017-05-21 16:38:20 -0600

    FoodTracker App
    Fully finished FoodTracker app for precourse work.

---
+++ FodTracker/MealTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MealTableViewCell.swift
+//  FodTracker
+//
+//  Created by Quinn Moreland on 5/20/17.
+//  Copyright © 2017 Quinn Moreland. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
